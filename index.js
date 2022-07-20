70d1966ec4f87c83d21acb30592b7ee5378d1626
Serhii <khomshenko@gmail.com>
2022-07-20 15:17:19 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('Запускаем таймер');
+
+alert('5');
+alert('4');
+alert('3');
+alert('2');
+alert('1');
+alert('0');
+
+console.log('Обратный отсчет закончен');
+const myName = Serhii;
+const mySurname = Mayko;
+let myFavoriteDrink = tea;
+let myFavoriteAnimal = dog;
+let myFavoriteProgrammingLanguage = JS;
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+
+
+let nameOfPerson='Alex';
+nameOfPerson=89;
+alert(nameOfPerson);
+
+let ageOfPerson=23;
+ageOfPerson='23';
+alert(ageOfPerson);
+
+let isAWoman=true;
+isAwoman=null;
+alert(isAWoman);
+
+
+
+
